e0970a920e2ae0be314a5758e5a4b35855ec8143
MaximBh <apelsin4321@yandex.ru>
2026-01-10 21:11:50 +0300

    управление звуками

---
+++ sound.py
@@ -0,0 +1,5 @@
+import arcade
+
+shoot = arcade.load_sound("assets/sounds/shoot.wav")
+explosion = arcade.load_sound("assets/sounds/explosion.wav")
+game_over = arcade.load_sound("assets/sounds/game_over.wav")
